91a79f29d138a5ebc547247c67ff03acb856c6f0
Matias <matias.martinez90@gmail.com>
2023-07-30 18:59:05 -0300

    Siguiente Commit

--- bd/category.go
+++ bd/category.go
@@ -4,37 +4,40 @@ import (
 	"database/sql"
 	"fmt"
 
-	//	"strconv"
-	//	"strings"
+	//"strconv"
 	"github.com/gambit/models"
-	//	"github.com/gambit/tool"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func InsertCategory(c models.Category) (int64, error) {
-	fmt.Println("Comienza Registro de InsertCategory")
+	fmt.Println("Inicializando funcion  db.InsertCategory")
 
 	err := DbConnect()
 	if err != nil {
 		return 0, err
 	}
+
 	defer Db.Close()
 
-	sentencia := "INSERT INTO category (Categ_Name, Categ_Path) VALUES ('" + c.CategName + "','" + c.CategPath + "')"
+	sentencia := "INSERT INTO category (Categ_Name, Categ_Path ) Values('" + c.CategName + "','" + c.CategPath + "')"
 
+	fmt.Println("ejecutnado sentencia")
+	fmt.Println(sentencia)
 	var result sql.Result
+
 	result, err = Db.Exec(sentencia)
 	if err != nil {
 		fmt.Println(err.Error())
 		return 0, err
 	}
+
 	LastInsertId, err2 := result.LastInsertId()
 	if err2 != nil {
+		fmt.Println(err.Error())
 		return 0, err2
 	}
 
 	fmt.Println("Insert Category > Ejecucion Exitosa")
-
 	return LastInsertId, nil
 
 }
