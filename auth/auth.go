d5037615c18053421506171465dfb6ea9ecf2eb1
Matias <matias.martinez90@gmail.com>
2023-08-17 21:47:10 -0300

    Siguiente Commit

--- auth/auth.go
+++ auth/auth.go
@@ -25,7 +25,7 @@ type TokenJSON struct {
 func ValidoToken(token string) (bool, error, string) {
 	fmt.Println("Ingresando a la funcion ValidoToken")
 	parts := strings.Split(token, ".")
-	parts2 := "eyJzdWIifQ=="
+	parts2 := "c3Vi"
 
 	if len(parts) != 3 {
 		fmt.Println("El token no es valido")
