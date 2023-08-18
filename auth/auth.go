c16416b6987b8c4d783d0894f27d49b9b6ec9c10
Matias <matias.martinez90@gmail.com>
2023-08-17 21:18:01 -0300

    Siguiente Commit

--- auth/auth.go
+++ auth/auth.go
@@ -34,6 +34,7 @@ func ValidoToken(token string) (bool, error, string) {
 	userInfo, err := base64.StdEncoding.DecodeString(parts[1])
 	if err != nil {
 		fmt.Println("No se puede decodificar la parte del token : ", err.Error())
+		fmt.Println(parts)
 		return false, err, err.Error()
 	}
 
