5d6dc39555bdc657c888280a5f02c220d53814c1
Matias <matias.martinez90@gmail.com>
2023-08-23 18:53:04 -0300

    Siguiente Commit

--- routers/category.go
+++ routers/category.go
@@ -2,9 +2,10 @@ package routers
 
 import (
 	"encoding/json"
+	"fmt"
 	"strconv"
 
-	//"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/gambit/bd"
 	"github.com/gambit/models"
 )
@@ -81,4 +82,37 @@ func DeleteCategory(body string, User string, id int) (int, string) {
 	}
 
 	return 200, "Delete OK"
+}
+
+func SelectCategories(body string, request events.APIGatewayV2HTTPRequest) (int, string) {
+	fmt.Println("Inicializando funcion  router.SelectCategories")
+
+	var err error
+	var CategId int
+	var Slug string
+
+	if len(request.QueryStringParameters["categId"]) > 0 {
+		CategId, err = strconv.Atoi(request.QueryStringParameters["categId"])
+		if err != nil {
+			return 500, "Ocurrio un error al intentar convertir el valor" + request.QueryStringParameters["categId"]
+		}
+
+	} else {
+		if len(request.QueryStringParameters["slug"]) > 0 {
+			Slug = request.QueryStringParameters["slug"]
+		}
+	}
+
+	lista, err2 := bd.SelectCategories(CategId, Slug)
+	if err2 != nil {
+		return 400, "Ocurrio un error al intentar capturar Categoria/s > " + err2.Error()
+	}
+
+	Categ, err3 := json.Marshal(lista)
+	if err3 != nil {
+		return 400, "Ocurrio un error al intentar convertir en JSON  Categoria/s > " + err3.Error()
+	}
+
+	return 200, string(Categ)
+
 }
